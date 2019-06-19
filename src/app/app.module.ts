a311149d396b707379d4ce85c4e5b1593db836d8
Ranjitha <ranjitha.amittisoft.com>
2019-06-20 02:50:16 +0530

    Angular events

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule }   from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
@@ -16,7 +16,8 @@ import { NavbarComponent } from './components/navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -9,82 +9,68 @@ import { User } from 'src/app/models/users';
 export class UsersComponent implements OnInit {
 
   users: User[];
-  showExtended: boolean = true;
+  showExtended: boolean = false;
   loaded: boolean = false;
-  enableAdd:boolean = true;
-  currentClasses = {};
-  currentStyles = {};
-
+  enableAdd: boolean = true;
+  showUserForm: boolean = false;
+  user: User = {
+    firstName: '',
+    lastName: '',
+    email: ''
+  }
   constructor() { }
 
   ngOnInit() {
-      this.users = [
-        {
-          firstName: 'Angular',
-          lastName: 'Node',
-          age: 20,
-          address: {
-            street: 'Bangalore',
-            city: 'Bangalore',
-            state: 'karnataka'
-          },
-          image: 'http://lorempixel.com/600/600/people/3',
-          isActive: false,
-          balance: 100,
-          registered: new Date('01/02/2019 08:30:00')
-        },
-        {
-          firstName: 'React',
-          lastName: 'Node',
-          age: 20,
-          address: {
-            street: 'Mysore',
-            city: 'Mysore',
-            state: 'karnataka'
-          },
-          image: 'http://lorempixel.com/600/600/people/2',
-          isActive: true,    
-          balance: 200,
-          registered: new Date('01/02/2019 08:30:00')
-        },
-        {
-          firstName: 'Vue',
-          lastName: 'Node',
-          age: 20,
-          address: {
-            street: 'Mandya',
-            city: 'Mandya',
-            state: 'karnataka'
-          },
-          image: 'http://lorempixel.com/600/600/people/1',
-          isActive: false,      
-          balance: 100,
-          registered: new Date('01/02/2019 08:30:00')
-        }
-      ];
-      this.loaded = true;
-      this.setCurrentClasses();
-    // this.showExtended = false;
-    this.addUser({
-      firstName: 'David',
-      lastName: 'Jackson'
-    })
-    
+    this.users = [
+      {
+        firstName: 'Angular',
+        lastName: 'Node',
+        email: 'aaa@gmail.com',
+        isActive: false,
+
+        registered: new Date('01/02/2019 08:30:00'),
+        hide: true
+      },
+      {
+        firstName: 'React',
+        lastName: 'Node',
+        email: 'bbb@gmail.com',
+        isActive: true,
+
+        registered: new Date('01/02/2019 08:30:00'),
+        hide: true
+      },
+      {
+        firstName: 'Vue',
+        lastName: 'Node',
+        email: 'ccc@gmail.com',
+        isActive: false,
+        registered: new Date('01/02/2019 08:30:00'),
+        hide: true
+      }
+    ];
+    this.loaded = true;
+
   }
 
-  addUser(user : User){
-    this.users.push(user);
-  }
-
-  setCurrentClasses(){
-    this.currentClasses = {
-      'btn-success': this.enableAdd,
-      'big-text': this.showExtended
+  addUser() {
+    this.user.isActive = true;
+    this.user.registered = new Date();
+    this.users.unshift(this.user);
+    this.user = {
+      firstName: '',
+      lastName: '',
+      email: ''
     }
   }
-  setcurrentStyles(){
-    this.currentStyles = {
-      'padding-top': this.showExtended ? '0' : '40px'
-    }
+  toggleHide(user: User) {
+    user.hide = !user.hide;
+  }
+  onSubmit(e) {
+    //console.log(13);
+    e.preventDefault();
+  }
+  fireEvent(e) {
+    console.log(e.target.value);
   }
 }
